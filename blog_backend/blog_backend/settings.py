e9601e49329c9b35bc409f268a65537ecab4a031
VDA-001 <vdavishu@gmail.com>
2021-04-26 11:28:55 +0530

    Generated new secret key and updated settings.py

--- blog_backend/blog_backend/settings.py
+++ blog_backend/blog_backend/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$t3vt1dbxnw*b%rw9uw9nap3#p0r*&yp1+qe+894uurf5lws6^'
-
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
