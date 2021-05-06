36a820c0ac1baf89272734e2232e715f39b2f94e
VDA-001 <vdavishu@gmail.com>
2021-05-06 12:28:00 +0530

    Post model created

--- blog_backend/api/post/models.py
+++ blog_backend/api/post/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from api.user.models import CustomUser
 
-# Create your models here.
+# Trial Model - 1
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'images/',blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(CustomUser,related_name='post',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title;
+
+    @property
+    def number_of_comments(self):
+        return BlogPostComment.objects.filter(blogpost_connected=self).count()
+
+class BlogPostComment(models.Model):
+    blogpost_connected = models.ForeignKey(BlogPost,on_delete=models.CASCADE,related_name='comments')
+    author = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    content = models.TextField()
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.author)+", "+self.blogpost_connected.title[:40]
