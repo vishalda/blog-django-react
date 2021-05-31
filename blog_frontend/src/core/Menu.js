06d56d598f4305db45069ad7101452b7714bd1cb
VDA-001 <vdavishu@gmail.com>
2021-05-31 12:55:41 +0530

    Added Bootstrap to react

--- blog_frontend/src/core/Menu.js
+++ blog_frontend/src/core/Menu.js
@@ -2,26 +2,25 @@ import React from 'react';
 import {withRouter,Link} from 'react-router-dom';
 import {signout,IsAuthenticated} from "../auth/helper/index";
 
-
 const Menu = () =>{
     return(
         <div>
-                <nav>
-                    <li><Link to="/">Home</Link></li>
-                    {!IsAuthenticated() && <div>
-                        <li><Link to="/register">Register</Link></li>
-                        <li><Link to="/login">Login</Link></li>
-                    </div>
-                    }
-                    <li><Link to="/post">explore</Link></li>
-                    <li><Link to="/category">Category</Link></li>
-                    {IsAuthenticated() &&
-                    <div>
-                        <li><Link to="/create-post">Create-Post</Link></li>
-                        <li><a href="/" onClick={()=>signout()}>Logout</a></li>
-                        <li><a href="/profile" >Profile</a></li>
-                    </div>}
-                </nav>
+            <nav>
+                <li><Link to="/">Home</Link></li>
+                {!IsAuthenticated() && <div>
+                    <li><Link to="/register">Register</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </div>
+                }
+                <li><Link to="/post">explore</Link></li>
+                <li><Link to="/category">Category</Link></li>
+                {IsAuthenticated() &&
+                <div>
+                    <li><Link to="/create-post">Create-Post</Link></li>
+                    <li><a href="/" onClick={()=>signout()}>Logout</a></li>
+                    <li><a href="/profile" >Profile</a></li>
+                </div>}
+            </nav>
         </div>
     );
 };
--- blog_frontend/src/index.js
+++ blog_frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
