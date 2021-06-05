8dea3869364ac3f3601ca7a87e0042b45766213f
VDA-001 <vdavishu@gmail.com>
2021-06-05 11:31:09 +0530

    Added css to CreatePost page

--- blog_frontend/src/core/CreatePost.js
+++ blog_frontend/src/core/CreatePost.js
@@ -86,36 +86,18 @@ class CreatePost extends React.Component{
         return(
             <div>
                 <Base />
+                <br/>
                 <Container>
                 <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                    </Form>
-                    <form>
-                        <li>title : </li>
-                        <input value = {this.state.title} onChange={this.handleChange("title")} type="text" required/>
-                        <li>description : </li>
-                        <input value = {this.state.description} onChange={this.handleChange("description")} type="text" required/>
-                        <li>body : </li>
+                    <Form.Group>
+                        <Form.Label>Title:</Form.Label>
+                        <Form.Control type="text" placeholder="Title..."  value = {this.state.title} onChange={this.handleChange("title")} required/>
+                        <Form.Label>Description:</Form.Label>
+                        <Form.Control type="text" placeholder="Description..." value = {this.state.description} onChange={this.handleChange("description")}/>
+                        <Form.Label>Body:</Form.Label>
                         <CKEditor
                             editor={ ClassicEditor }
-                            data="<p>Hello from CKEditor 5!</p>"
+                            data={this.state.body}
                             onReady={ editor => {} }
                             onChange={ ( event, editor ) => {
                                 const data = editor.getData();
@@ -123,14 +105,15 @@ class CreatePost extends React.Component{
                             } }
                             onBlur={ ( event, editor ) => {} }
                             onFocus={ ( event, editor ) => {} }
-                        />
-                        {/*Setting image value as this.state.image gives InvalidStateError */}
+                        /><br/>
                         <img src={this.state.imageViewer} alt="" style={{width:"500px"}}/>
-                        <input  value = {undefined} type="file" onChange={this.handleChange('image')} required/>
-                        {/*Setting value using category_id -1 as the value of actual category_id starts from 1 */}
+                        <Form.File id="exampleFormControlFile1" label="Upload an Image" value = {undefined} type="file" onChange={this.handleChange('image')} required/><br/>
                         <Select value={this.state.category_options[this.state.category_id-1]} options={this.state.category_options} onChange={this.handleChange('category_id')} required/>
-                        <button onClick={this.onSubmit}>submit</button>
-                    </form>
+                    </Form.Group>
+                    <Button variant="primary" type="submit" onClick={this.onSubmit}>
+                        Submit
+                    </Button>
+                    </Form>
                 </Container>
                 
             </div>
