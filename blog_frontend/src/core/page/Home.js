354ba230a53af081d83cddfe72bbedcabbc960eb
VDA-001 <vdavishu@gmail.com>
2021-06-13 17:49:20 +0530

    Included source code link

--- blog_frontend/src/core/page/Home.js
+++ blog_frontend/src/core/page/Home.js
@@ -6,6 +6,7 @@ import Button from 'react-bootstrap/Button';
 import { Redirect } from 'react-router';
 import { BsHeartFill } from "react-icons/bs";
 import { AiFillGithub,AiFillInstagram,AiFillLinkedin } from "react-icons/ai";
+import { FaGithubSquare } from "react-icons/fa";
 
 class Home extends React.Component{
     performRedirect=()=>{
@@ -17,7 +18,7 @@ class Home extends React.Component{
                 <Base/>
                 <div className="home-page-content">
                     <h1>Welcome!</h1>
-                    <h4>A simple Blog which has basic CRUD functionality for editing posts.<br/><br/> Interested in publishing a new post?</h4>
+                    <h4>A simple Blog which has basic CRUD functionality for editing posts.<br/><br/>Source code : <a href='https://github.com/VDA-001/blog-django-react'><FaGithubSquare style={{color:'rgb(31, 31, 31)'}}/></a><br/><br/> Interested in publishing a new post?</h4>
                     <Button className="button" onClick={this.performRedirect()}>Create-Post</Button>
                 </div>
                 <div className="home-page-svg"><HomePageSvg/></div>
