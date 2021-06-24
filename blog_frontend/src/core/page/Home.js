57376df317fbe39e68e6bcc226e0ad05c8c04c0b
VDA-001 <vdavishu@gmail.com>
2021-06-24 12:58:51 +0530

    Update Home page -2

--- blog_frontend/src/core/page/Home.js
+++ blog_frontend/src/core/page/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Redirect } from 'react-router';
+import {Link} from 'react-router-dom';
 import Base from '../components/Base';
 import {HomePageSvg} from "../components/svg";
 import Button from 'react-bootstrap/Button';
 import { BsHeartFill } from "react-icons/bs";
 import { AiFillGithub,AiFillInstagram,AiFillLinkedin } from "react-icons/ai";
 import { FaGithubSquare } from "react-icons/fa";
+import { RiSunFill,RiMoonFill } from "react-icons/ri";
 import "../../SCSS/home.scss";
+import { IsAuthenticated } from '../../auth/helper';
 
 class Home extends React.Component{
     constructor(props){
@@ -31,10 +34,6 @@ class Home extends React.Component{
             document.body.style.color = "black";
         }
     }
-    
-    performRedirect=()=>{
-        return <Redirect to='/create-post' />;
-    }
 
     render(){
         return(
@@ -44,9 +43,20 @@ class Home extends React.Component{
 
                 <div className="home-page-content">
                     <h1>Welcome!</h1>
-                    <h4>A simple Blog which has basic CRUD functionality for editing posts.<br/><br/>Source code : <a href='https://github.com/VDA-001/blog-django-react'><FaGithubSquare style={{color:'rgb(31, 31, 31)'}}/></a><br/><br/> Interested in publishing a new post?</h4>
-                    <Button className="button" onClick={this.performRedirect()}>Create-Post</Button>
-                    <Button className="toggle-theme" onClick={()=>{
+                    {IsAuthenticated() && 
+                        <><h4>A simple Blog which has basic CRUD functionality for editing posts.<br/><br/> Interested in publishing a new post? <Link to="/create-post" style={{color:'#6c63ff'}}>Create-Post</Link></h4><br/><br/></>
+                    }
+                    {!IsAuthenticated() &&
+                        <><h4>A simple Blog which has basic CRUD functionality for editing posts.<br/><br/> Join us. <Link to="/register" style={{color:'#6c63ff'}}>Register</Link></h4><br/><br/></>
+                    }
+                    <h5>
+                        Source code : &nbsp;
+                        <a href='https://github.com/VDA-001/blog-django-react'><FaGithubSquare className={localStorage.getItem('dark') === "true" ? "text-light github-icon" : "text-dark github-icon"}/></a>
+                    </h5>
+                    
+                </div>
+                <div className={localStorage.getItem('dark')==="true" ? "footer dark-theme-footer" : "footer"}>
+                    <button className="toggle-theme-button" onClick={()=>{
                             this.setState({darkTheme:!this.state.darkTheme})
                             localStorage.setItem("dark",JSON.stringify(this.state.darkTheme));
                             if(localStorage.getItem('dark')==="true"){
@@ -61,9 +71,10 @@ class Home extends React.Component{
                             return(
                                 <Redirect to="/" />
                             );
-                        }}>Darktheme</Button>
-                </div>
-                <div className={localStorage.getItem('dark')==="true" ? "footer dark-theme-footer" : "footer"}>
+                        }}>{localStorage.getItem("dark")==="true" ? 
+                            <RiSunFill style={{color:'white'}} className="theme-icon"/>
+                         : <RiMoonFill style={{color:'black'}} className="theme-icon"/>}
+                    </button>
                     <h5 style={{position:'relative',float:'left'}}>Made with <BsHeartFill style={{color:'red'}}/>  by Vishal</h5>
                     <h5 style={{position:'relative',float:'right'}}>Connect with me : <a href='https://github.com/VDA-001'><AiFillGithub className="icons"/></a><a href='https://www.instagram.com/_v_ishhh_/'><AiFillInstagram className="icons"/></a><a href='https://www.linkedin.com/in/vishal-da-9216091a9/'><AiFillLinkedin className="icons"/></a></h5>
                 </div>
