5648c23d113e64c3f0f13b65c2401e377d5fbe90
VDA-001 <vdavishu@gmail.com>
2021-06-27 21:59:13 +0530

    Cleaning up the code

--- blog_backend/api/migrations/0001_initial.py
+++ blog_backend/api/migrations/0001_initial.py
@@ -1,15 +1,16 @@
 from django.db import migrations
 from api.user.models import CustomUser
+import os
 
 class Migration(migrations.Migration):
     def seed_data(apps, schema_editor):
-        user = CustomUser(name = "Vishal",
-            email = "vda.vishal@gmail.com",
-            username = "VDA_001",
+        user = CustomUser(name = str(os.getenv('SUPERUSER_NAME')),
+            email = str(os.getenv('SUPERUSER_EMAIL')),
+            username =str(os.getenv('SUPERUSER_USERNAME')),
             is_staff=True,
             is_superuser=True,
             phone = "9876543210",)
-        user.set_password("qwert")
+        user.set_password(str(os.getenv('SUPERUSER_PASSWORD')))
         user.save()
     
     dependencies = [
