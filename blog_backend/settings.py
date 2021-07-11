4f0e9f1c6bb1b354081624566b58cbcf993a79b6
Vishal D A <67089703+VDA-001@users.noreply.github.com>
2021-07-11 15:36:00 +0530

    Update settings.py

--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'blog_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
