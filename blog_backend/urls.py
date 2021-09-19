dbb3408c0470d42c1e40be98ec4c13d5eb9d215b
vishalda <vdavishu@gmail.com>
2021-09-19 23:35:06 +0530

    Fixed image not found bug and updated dependencies

--- blog_backend/urls.py
+++ blog_backend/urls.py
@@ -23,7 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/',include('api.urls')),
-    re_path(r'^',FrontendAppView.as_view())
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
